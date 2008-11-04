40d39956dd5b7cd17ec20c540f21a2ed5497b8bc
corryn <corryn@b2e75cc2-c23a-0410-bd76-b3583853a6a7>
2008-11-04 17:06:55 +0000

    added some code to run.sh, as well as the messages folder - Jergas
    git-svn-id: https://svn.code.sf.net/p/ouroborus/code@47 b2e75cc2-c23a-0410-bd76-b3583853a6a7

--- birdcage/run.sh
+++ birdcage/run.sh
@@ -1,1 +1,7 @@
+cat messages/0.txt >> log.txt
+cat messages/1.txt >> log.txt
+export OPCODEDIR64=/usr/lib/csound/plugins64
+cat messages/2.txt >> log.txt
+echo $OPCODEDIR64 >> log.txt
+cat messages/3.txt >> log.txt
 python main_sequence.py build_ext --inplace 2>> log.txt
