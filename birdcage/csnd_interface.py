a0185803f7099f5ada025592f26176826b1e4378
el_erno <el_erno@b2e75cc2-c23a-0410-bd76-b3583853a6a7>
2009-05-15 12:19:31 +0000

    Did not update csnd_interface on previous commit.
    git-svn-id: https://svn.code.sf.net/p/ouroborus/code@102 b2e75cc2-c23a-0410-bd76-b3583853a6a7

--- birdcage/csnd_interface.py
+++ birdcage/csnd_interface.py
@@ -6,21 +6,24 @@ from random import choice, randint, random
 # Csound API library.
 import csnd
 # Sound-related submodules.
-import csd_generator as csdGenerator
+import Csnd_data as CsndData
 
-## Instantiate Csound's API, and it's performance class.
-cSnd	= csnd.CppSound()
-perf	= csnd.CsoundPerformanceThread(cSnd)
+## Instantiate Csound's API, it's performance class and a .csd
+#generator.
+cSnd			= csnd.CppSound()
+perf			= csnd.CsoundPerformanceThread(cSnd)
+CsdGenerator	= CsndData.CsdGenerator(39) # the (hard-wired) number
+											#of csound background
+											#instruments is 39.
 
 
 def initCSnd():
-	"""Generates a single-file Csound structured data (CSD) string,
+	"""Generates a single-file Csound Structured Data (.csd) string,
 	sets it in the Csound API, exports it for performance, compiles it,
 	and starts the 	performance.
 	"""
 	#cSnd.setPythonMessageCallback() # useful for debugging.
-	csd = csdGenerator.makeCSD(39) # the (hard-wired) number of csound
-									# background instruments is 39.
+	csd = CsdGenerator.csd
 	cSnd.setCSD(csd)
 	cSnd.exportForPerformance()
 	cSnd.compile()
