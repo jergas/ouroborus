b12e61e19da8ece67f9da8122e1577ab3dcd0add
cincoinister <cincoinister@b2e75cc2-c23a-0410-bd76-b3583853a6a7>
2010-03-22 18:57:31 +0000

    The logic has been slightly corrected by Jergas but the neighbor count is still down
    git-svn-id: https://svn.code.sf.net/p/ouroborus/code@199 b2e75cc2-c23a-0410-bd76-b3583853a6a7

--- birdcageLibrary/environment.py
+++ birdcageLibrary/environment.py
@@ -15,6 +15,8 @@ def getEnvironment(port):
 	""" Gets a reading from the environment sensor. In case there is no such
 	device, prints a notification of such a lack.
 	"""
+	## The try except clause is used in order to be able to run the code
+     # without an arduino interface.
 	try:
 		sensor =  serial.Serial(str(port), 9600)
 		environment = []
@@ -67,53 +69,51 @@ def visuals(input_list,side,environment,generation):
 	im.save("generacion"+str(generation)+".tiff")
 
 def updateRule(input_list,side,environment):
-    ONneighbours = 0
-    espacio2 ={}
-        
-    for cell in input_list:
-    	if abs(abs(cell) - environment) <= 5:
-    		cell = abs(cell)
-    	else:
-    		cell = -abs(cell)
-	    		
-    for i in range(0,side):
-        for j in range(0,side):
-            if (side-1)>i>0 and (side-1)>j>0:
-                for (k,l) in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]:
-			if input_list[(side*(i+k)) + (j+l)] > 0:
-				ONneighbours = ONneighbours + 1
-                
-                if input_list[(side*i) + j] >= 0:
-                    if 2 <= ONneighbours <= 3:
-                        espacio2[i,j] = environment
-                    else:
-                    	if (input_list[(side*i)+j]-environment) > 0:
-                            espacio2[i,j] = environment - 15
-                        elif (input_list[(side*i)+j]-environment) < 0:
-                            espacio2[i,j] = environment + 15
-			else:
-                            if input_list[(side*i)+j]<500:
-                                espacio2[i,j]= environment+15
-                            if input_list[(side*i)+j]>500:
-                                espacio2[i,j]= environment-15
+     espacio2 ={}
+     print environment
+     for cell in input_list:
+          print cell
+          if abs(abs(cell) - environment) <= 5:
+               cell = abs(cell)
+          else:
+    	          cell = -abs(cell)
+    	
+     for i in range(0,side):
+          for j in range(0,side):
+               ONneighbors = 0
+               if (side-1)>i>0 and (side-1)>j>0:
+                    for (k,l) in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]:
+                         if input_list[(side*(i+k)) + (j+l)] >= 0:
+                              ONneighbors = ONneighbors + 1
+                    #print ONneighbors
+                    if input_list[(side*i) + j] >= 0:
+                         if 2 <= ONneighbors <= 3:
+                              espacio2[i,j] = environment
+                         elif (input_list[(side*i)+j]-environment) > 0:
+                              espacio2[i,j] = environment - 15
+                         elif (input_list[(side*i)+j]-environment) < 0:
+                              espacio2[i,j] = environment + 15
+                         else:
+                              if input_list[(side*i)+j]<500:
+                                   espacio2[i,j]= environment+15
+                              if input_list[(side*i)+j]>500:
+                                   espacio2[i,j]= environment-15
                     	
-                elif input_list[(side*i)+j] < 0:
-                    if ONneighbours == 3:
-                        espacio2[i,j] = environment
+               else:
+                    if ONneighbors == 3:
+                         espacio2[i,j] = environment
                     else:
                         espacio2[i,j]= (abs(input_list[(side*i)+j]))
 
-    for i in range(1,side-1):
-        for j in range(1,side-1):
-		input_list[(side*i)+j] = espacio2[i,j]
+     for i in range(1,side-1):
+          for j in range(1,side-1):
+               input_list[(side*i)+j] = espacio2[i,j]
     
-    return input_list 
+     return input_list 
            
 
 
 sideL = 100
-## The try except clause is used in order to be able to run the code
-# without an arduino interface.
 environment = getEnvironment('/dev/ttyUSB0')
 cells = initials(0.15,sideL,environment)
 
@@ -121,5 +121,5 @@ for time in range(0,300):
 	visuals(cells,sideL,environment,time)
 	cells = updateRule(cells,sideL,environment)
 	environment = getEnvironment('/dev/ttyUSB0')
-	print time,environment
+	#print time,environment
 
