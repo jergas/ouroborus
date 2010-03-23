342eb96bde8962304009919baae923efef6f79da
cincoinister <cincoinister@b2e75cc2-c23a-0410-bd76-b3583853a6a7>
2010-03-23 22:13:23 +0000

    The update rule for environment.py is now working propperly. The update rule has been moved to a module of it's own (rule_environment.pyx) and is now called as a .so by environment.py.
    git-svn-id: https://svn.code.sf.net/p/ouroborus/code@203 b2e75cc2-c23a-0410-bd76-b3583853a6a7

--- birdcageLibrary/rule_environment.pyx
+++ birdcageLibrary/rule_environment.pyx
@@ -1,22 +1,26 @@
 def updateRule(input_list,side,environment):
-     espacio2 ={}
-     print environment
-     for cell in input_list:
-          print cell
-          if abs(abs(cell) - environment) <= 5:
-               cell = abs(cell)
-          else:
-    	          cell = -abs(cell)
-    	
-     for i in range(0,side):
+     '''This module contains the rules for updating the automaton based on each cells
+     neighbors and their response value. The neighbor rules are the same as Conways
+     game of life but they have been adapted to interact with the response value of
+     each cell.
+     '''
+     espacio2 = {} #A dictionary in which the updated state of each cell is stored
+     for i in range(0,side): #Define the state of the cell (+ = alive - = dead)
+          for j in range(0,side):
+               if abs(abs(input_list[(side*i) + j]) - environment) <= 5:
+                    input_list[(side*i) + j] = abs(input_list[(side*i) + j])
+               else:
+                    input_list[(side*i) + j] = -abs(input_list[(side*i) + j])
+
+     for i in range(0,side): #Check each cell
           for j in range(0,side):
                ONneighbors = 0
                if (side-1)>i>0 and (side-1)>j>0:
-                    for (k,l) in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]:
-                         if input_list[(side*(i+k)) + (j+l)] >= 0:
+                    for (k,l) in [(-1,-1), (-1,0), (-1,1), (0,-1), (0,1), (1,-1), (1,0), (1,1)]:# Define neighbors
+                         if input_list[(side*(i+k)) + (j+l)] >= 0:#count living neighbours
                               ONneighbors = ONneighbors + 1
-                    #print ONneighbors
-                    if input_list[(side*i) + j] >= 0:
+                    
+                    if input_list[(side*i) + j] >= 0:#if cell is alive
                          if 2 <= ONneighbors <= 3:
                               espacio2[i,j] = environment
                          elif (input_list[(side*i)+j]-environment) > 0:
@@ -26,16 +30,16 @@ def updateRule(input_list,side,environment):
                          else:
                               if input_list[(side*i)+j]<500:
                                    espacio2[i,j]= environment+15
-                              if input_list[(side*i)+j]>500:
+                              else:
                                    espacio2[i,j]= environment-15
-                    	
-               else:
-                    if ONneighbors == 3:
-                         espacio2[i,j] = environment
-                    else:
-                        espacio2[i,j]= (abs(input_list[(side*i)+j]))
 
-     for i in range(1,side-1):
+                    else: #if cell is dead
+                         if ONneighbors == 3:
+                              espacio2[i,j] = environment
+                         else:
+                              espacio2[i,j]= (abs(input_list[(side*i)+j]))
+
+     for i in range(1,side-1):#Update the automaton
           for j in range(1,side-1):
                input_list[(side*i)+j] = espacio2[i,j]
     
