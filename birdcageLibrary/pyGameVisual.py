ce392b22960ff2aac9545f7608cd15acee6605f9
cincoinister <cincoinister@b2e75cc2-c23a-0410-bd76-b3583853a6a7>
2010-06-03 16:54:50 +0000

    Modified the __init__ so that the buffer.tiff binaries are not required in the distribution.
    git-svn-id: https://svn.code.sf.net/p/ouroborus/code@245 b2e75cc2-c23a-0410-bd76-b3583853a6a7

--- birdcageLibrary/pyGameVisual.py
+++ birdcageLibrary/pyGameVisual.py
@@ -19,13 +19,13 @@ class PyGVisual():
           self.height = height
           self.screen = pygame.display.set_mode((self.width,self.height))
           pygame.display.set_caption("Circadian Viewer")
-          self.image = pygame.image.load("buffer.tiff").convert()
           self.fullscreen = False
           
           
      def generate(self, automaton, environment, side=100, output='buffer.tiff'):
           '''This function generates a buffer image that will be the main visual feature of the viewer
           it requires an automaton coded as a list, an environment value and an image size (default is 100).'''
+         
           self.picture = Image.new("RGB", (side,side))
           self.draw = ImageDraw.Draw(self.picture)
                     
@@ -39,7 +39,6 @@ class PyGVisual():
           self.picture = self.picture.resize((8*self.picture.size[0],8*self.picture.size[1]), Image.NEAREST)
           self.picture.save(output)
           
-
      def update(self,inputImage='buffer.tiff'):
           '''This function is required in order to update the screen'''
           self.image = pygame.image.load(inputImage).convert()
