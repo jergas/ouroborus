e5188a0be8d2ec464d8dde7b4d2091f3515dc906
el_erno <el_erno@b2e75cc2-c23a-0410-bd76-b3583853a6a7>
2010-07-27 22:09:40 +0000

    Deleted obsolete files.
    git-svn-id: https://svn.code.sf.net/p/ouroborus/code@296 b2e75cc2-c23a-0410-bd76-b3583853a6a7

--- birdcage/run.sh
+++
@@ -1,7 +0,0 @@
-cat messages/0.txt >> log.txt
-cat messages/1.txt >> log.txt
-export OPCODEDIR64=/usr/lib/csound/plugins64
-cat messages/2.txt >> log.txt
-echo $OPCODEDIR64 >> log.txt
-cat messages/3.txt >> log.txt
-python main_sequence.py 2>> log.txt
