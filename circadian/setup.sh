9d240be7171a3fe45998aeaeaf42b08dd52baff8
corryn <corryn@b2e75cc2-c23a-0410-bd76-b3583853a6a7>
2010-08-13 16:48:04 +0000

    circadian needs to have code.so in its own directory as there is a module with the same name in the standard library, we need to fix that, but for now, this hack will do
    git-svn-id: https://svn.code.sf.net/p/ouroborus/code@316 b2e75cc2-c23a-0410-bd76-b3583853a6a7

--- circadian/setup.sh
+++ circadian/setup.sh
@@ -2,8 +2,13 @@ pushd ../birdcage/
 sudo python setup.py install
 popd
 
+pushd ../worldish/
+./compile.sh
+popd
+
 rm heru*
 
+cp ../worldish/code.so .
 cp ../worldish/GOD.py .
 cp ../worldish/visual.py .
 cp ../worldish/sound.py .
@@ -15,5 +20,9 @@ cp ../worldish/Numeric_utils.py .
 cp ../worldish/equal_temper.py .
 cp ../worldish/Csnd_notes.py .
 cp ../worldish/Note_methods.py .
+
+cp ../birdcage/topology.pxd .
+cp ../birdcage/neighborhood.pxd .
+cp ../birdcage/rule.pxd .
 
 python2.5 setup_lib_environment.py build_ext --inplace
